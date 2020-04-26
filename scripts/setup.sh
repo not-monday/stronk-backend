a3d30e386517fba3096da9c413942893890a058b
kkjasoncheung <kkjason.cheung@mail.utoronto.ca>
2020-04-26 18:41:11 -0400

    implements auth module and updates setup.sh

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -5,7 +5,6 @@ export FLASK_APP=stronk
 export FLASK_ENV=development
 # TODO: Set GOOGLE_APPLICATION_CREDENTIALS to the path that contains your service account private key
 export GOOGLE_APPLICATION_CREDENTIALS=""
-export GOOGLE_APPLICATION_CREDENTIALS="stronk-df73b-firebase-adminsdk-gg4i5-86127a6e73.json"
 if [ "$GOOGLE_APPLICATION_CREDENTIALS" != "" ]; then
     flask db upgrade
 else
