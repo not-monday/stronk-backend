9a9154426f1941a532041fa8167eb16de7a371d4
kkjasoncheung <kkjason.cheung@mail.utoronto.ca>
2020-04-27 01:55:25 -0400

    Refactor verify_token

--- stronk/utils/auth.py
+++ stronk/utils/auth.py
@@ -3,15 +3,17 @@ from stronk.errors import errors as e
 
 
 def verify_token(id_token):
+    """Returns the uid for the user with id_token if id_token is valid and
+       not revoked."""
     try:
         # Verify the ID token while checking if the token is revoked by
         # passing check_revoked=True.
         decoded_token = auth.verify_id_token(id_token, check_revoked=True)
         # Token is valid and not revoked.
-        uid = decoded_token['uid']
+        return decoded_token['uid']
     except auth.RevokedIdTokenError:
         # Token revoked, inform the user to reauthenticate or signOut().
-        raise e.RevokedIdTokenError
+        raise e.RevokedIdTokenError("Token has been revoked.")
     except auth.InvalidIdTokenError:
         # Token is invalid
-        raise e.InvalidIdTokenError
+        raise e.InvalidIdTokenError("Token is not valid.")
