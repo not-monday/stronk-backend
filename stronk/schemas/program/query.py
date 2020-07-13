389f13b0257b2b7dfa9e3ca2450bdb15094b9a4b
13abel <therichardwei@gmail.com>
2020-07-13 11:54:55 -0400

    added querying by user

--- stronk/schemas/program/query.py
+++ stronk/schemas/program/query.py
@@ -1,8 +1,10 @@
 import graphene
 
 from stronk.models.program import Program as ProgramModel
+from stronk.models.user import User as UserModel
 from stronk.schemas.program.type import Program
-
+from stronk.errors.not_found import NotFound
+from stronk.constants import USER_NOT_FOUND_MSG
 
 class Query(graphene.ObjectType):
     programs = graphene.List(Program)
@@ -15,9 +17,9 @@ class Query(graphene.ObjectType):
         query = Program.get_query(info)
         return query.all()
 
-    def resolve_program(root, info, id: int = None, name: str = None, author: str = None):
-        """Search for program in decreasing order of precedence: id, name and
-           author. If an author created more than one program, a list of
+    def resolve_program(root, info, id: int = None, name: str = None, author: str = None, user_id:int = None, ):
+        """Search for program in decreasing order of precedence: id, name, author,
+           and user id. If an author created more than one program, a list of
            programs is returned."""
         query = Program.get_query(info)
         if id:
@@ -28,5 +30,12 @@ class Query(graphene.ObjectType):
 
         if author:
             return query.filter(ProgramModel.author == author).first()
+
+        if user_id:
+            user : UserModel = UserModel.find_by_id(user_id)
+            if not user:
+                raise NotFound(USER_NOT_FOUND_MSG)
+
+            return query.filter(ProgramModel.id == user.current_program).first()
 
         return None
