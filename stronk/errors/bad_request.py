a54e123074d3ba20867f532d2f426ce088c626da
kkjasoncheung <kkjason.cheung@mail.utoronto.ca>
2020-09-18 23:02:23 -0400

    Add query by program_id and username

--- stronk/errors/bad_request.py
+++ stronk/errors/bad_request.py
@@ -2,7 +2,7 @@ from stronk import constants as c
 from stronk.errors.base_exception import BaseException
 
 
-class BadAttributes(BaseException):
+class BadRequest(BaseException):
 
-    def __init__(self, message, code=c.BAD_ATTRIBUTES_ERROR_CODE):
+    def __init__(self, message, code=c.BAD_REQUEST_ERROR_CODE):
         super().__init__(message, code)
--- stronk/schemas/program_reviews/query.py
+++ stronk/schemas/program_reviews/query.py
@@ -1,27 +1,57 @@
 from flask import g
 import graphene
 
+from stronk.constants import PROGRAM_NOT_FOUND_MSG, USER_NOT_FOUND_MSG
+from stronk.errors.bad_request import BadRequest
+from stronk.models.program import Program as ProgramModel
 from stronk.models.program_reviews import ProgramReviews as ProgramReviewsModel
+from stronk.models.user import User as UserModel
 from stronk.schemas.program_reviews.type import ProgramReviews
 
-
 class Query(graphene.ObjectType):
-    program_reviews = graphene.List(ProgramReviews)
+    program_reviews = graphene.List(ProgramReviews,
+                                    program_id=graphene.Int(),
+                                    username=graphene.String())
     program_review = graphene.Field(ProgramReviews,
-                                    program_id=graphene.Int(required=True))
+                                    program_id=graphene.Int(required=True),
+                                    username=graphene.String(required=True))
 
-    def resolve_program_reviews(root, info):
-        """Get the program reviews made by the current user."""
+    def resolve_program_reviews(root,
+                                info,
+                                program_id: int = None,
+                                username: str = None):
+        """Get the program reviews by program or by user."""
         query = ProgramReviews.get_query(info)
+        if not (program_id or username):
+            raise BadRequest("Either program_id or username must be given.")
+        if username:
+            user = UserModel.find_by_username(username)
+            if not user:
+                raise NotFound(USER_NOT_FOUND_MSG)
+            return query.order_by(ProgramReviewsModel.created_at.desc()).filter(
+                ProgramReviewsModel.reviewer_id == user.id).all()
+            )
+        else:
+            program = ProgramModel.find_by_id(program_id)
+            if not program:
+                raise NotFound(PROGRAM_NOT_FOUND_MSG)
+            return query.order_by(ProgramReviewsModel.created_at.desc()).filter(
+                ProgramReviewsModel.program_id == program_id).all()
 
-        return query.order_by(ProgramReviewsModel.created_at.desc()).filter(
-            ProgramReviewsModel.reviewer_id == g.id).all()
-
-    def resolve_program_review(root, info, program_id: int):
-        """Get a program review made by the current user."""
+    def resolve_program_review(root, info, program_id: int, username: str):
+        """Get a program review made by a user for a program.
+        
+        program_id: The parent_id of the program.
+        username: User's username.
+        """
         query = ProgramReviews.get_query(info)
-
+        user = UserModel.find_by_username(username)
+        if not user:
+            raise NotFound(USER_NOT_FOUND_MSG)
+        program = ProgramModel.find_by_id(program_id)
+        if not program:
+            raise NotFound(PROGRAM_NOT_FOUND_MSG)
         return (query
-                .filter(ProgramReviewsModel.reviewer_id == g.id,
+                .filter(ProgramReviewsModel.reviewer_id == user.id,
                         ProgramReviewsModel.program_id == program_id)
                 .first())
